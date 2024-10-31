e2272cebc3ab9121d9abab03f2ab1e87fccedbd0
annous246 <anasrabhi0@gmail.com>
2024-10-31 20:43:19 +0100

    frontend struct commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const [p,setp]=useState<string>("azdaz")
+function App() {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
